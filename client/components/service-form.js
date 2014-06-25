2d3d4a1d69df2cf95c6b6076b2aee94e5f2a91fc
Eduardo Sorribas <eduardo@sorribas.org>
2014-06-25 22:47:53 +0200

    Change env vars part of the form to use arrays instead of objects.

--- client/components/service-form.js
+++ client/components/service-form.js
@@ -30,6 +30,10 @@ var ServiceForm = React.createClass({
 
     var service = xtend(this.state);
     service.remote = this.props.remote;
+    service.env = service.env.reduce(function(obj, envVar) {
+      obj[envVar.key] = envVar.value;
+      return obj;
+    }, {});
 
     if (this.props.id) {
       service.name = this.props.id;
@@ -38,38 +42,50 @@ var ServiceForm = React.createClass({
     superagent.post('/api/services').send(service).end(onsave);
   },
 
+  _setEnvs: function() {
+    if (!this.state.env || Array.isArray(this.state.env)) return;
+    var env = [];
+    var self = this;
+    Object.keys(this.state.env).forEach(function(key) {
+      env.push({key: key, value: self.state.env[key]});
+    });
+    this.setState({env: env});
+  },
+
   envs: function() {
     var rows;
     var self = this;
-    if (!this.state.env) rows =  <div />;
-    else rows = Object.keys(this.state.env).map(function(key) {
+    var i = 0;
+    if (!Array.isArray(this.state.env)) setTimeout(this._setEnvs.bind(this), 0);
 
+    if (!this.state.env || !this.state.env.length) rows =  <div />;
+    else rows = this.state.env.map(function(envVar) {
+      var index = i++;
       var changeKey = function() {
         var env = self.state.env;
-        env[event.target.value] = env[key];
-        delete env[key];
+        env[index].key = event.target.value;
         self.setState({env: env});
       };
 
       var change = function(event) {
         var env = self.state.env;
-        env[key] = event.target.value;
+        env[index].value = event.target.value;
         self.setState({env: env});
       };
 
       var remove = function() {
         var env = self.state.env;
-        delete env[key];
+        env.splice(index, 1);
         self.setState({env: env});
       };
 
       return (
         <div className="row">
           <div className="column large-4 small-4">
-            <input value={key} type="text" placeholder="Variable" onChange={changeKey}/>
+            <input value={self.state.env[index].key} type="text" placeholder="Variable" onChange={changeKey}/>
           </div>
           <div className="column large-6 small-6">
-            <input value={self.state.env[key]} type="text" placeholder="Value" onChange={change}/>
+            <input value={self.state.env[index].value} type="text" placeholder="Value" onChange={change}/>
           </div>
           <div className="column large-2 small-2">
             <a className="button alert" onClick={remove}>X</a>
@@ -79,8 +95,8 @@ var ServiceForm = React.createClass({
     });
 
     var addEnv = function() {
-      var env = self.state.env || {};
-      env[''] = '';
+      var env = self.state.env || [];
+      env.push({key: '', value: ''});
       self.setState({env: env});
     };
 
